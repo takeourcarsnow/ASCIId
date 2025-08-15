8669edb5053b4f8f93c685c4bb4de86c93c7b338
Ignas <ignasnefas@gmail.com>
2025-08-15 12:28:43 +0300

    Delete patterns directory

--- patterns/_template.js
+++
@@ -1,7 +0,0 @@
-function generatePattern(context, time) {
-    // Add this check at the start of each pattern generator
-    if(context.isBinary && context.chars.length !== 2) {
-        context.chars = ['0','1'];
-    }
-    // Rest of pattern code...
-} 
