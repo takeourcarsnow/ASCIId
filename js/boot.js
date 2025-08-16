035be67994aa66342869cebab99361163fd28da3
Ignas <ignasnefas@gmail.com>
2025-08-17 00:51:37 +0300

    Delete js directory

--- js/boot.js
+++
@@ -1,9 +0,0 @@
-function fit(){
-  const rect=document.getElementById('viewer').getBoundingClientRect();
-  canvas.style.width=rect.width+'px';
-  canvas.style.height=rect.height+'px';
-  resizeCanvas(); gridDirty=true;
-}
-window.addEventListener('resize',fit);
-fit();
-requestAnimationFrame(frame);
